519e7ee8628e3702b3124a4bf7479ef1ab773118
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-09 20:17:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,5 +3,8 @@ phrase = "I LOVE YOU GRANDMA!"
 def speak_to_grandma
   if phrase == "i love you grandma"
     return "HUH?! SPEAK UP, SONNY!"
-  elseif phrase == "Can hear you
-  return "
+  elseif phrase == "I love you Grandma!"
+  return "NO, NOT SINCE 1938!"
+else phrase == "I love you grandma!".upcase
+  return "I LOVE YOU TOO PUMPKIN!"
+end
